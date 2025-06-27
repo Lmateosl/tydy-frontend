a3493610ccfecab630cc92e623d43af8e22864ea
Lmateosl <mateosan67@hotmail..com>
2025-06-27 17:32:28 -0400

    PWA configurada

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [
+    react(), 
+    tailwind(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TyDy',
+        short_name: 'TyDy',
+        description: 'Simplifica, verifica y gestiona tareas fácilmente.',
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0A2A47",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
