cbb94c0853ac793d81d9b080f5cbde8e94c5ff5c
Lmateosl <mateosan67@hotmail..com>
2025-06-27 18:12:39 -0400

    Mejora PWA y modal de actualización

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -24,7 +24,7 @@ export default function Layout({ children }) {
         </div>
 
         {/* Contenido */}
-        <main className="flex-1 p-4 !overflow-auto bg-white max-w-full max-h-[87vh] md:max-h-fit md:mt-1 mt-[6vh] mb-[7vh] md:mb-1">
+        <main className="flex-1 p-4 !overflow-auto bg-white max-w-full max-h-[84vh] md:max-h-fit md:mt-1 mt-[6vh] mb-[10vh] md:mb-1">
           {children}
         </main>
 
